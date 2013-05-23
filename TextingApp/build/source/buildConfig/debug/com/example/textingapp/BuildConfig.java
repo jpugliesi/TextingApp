054718d0c4a9dbdbbcc1a811b5cb90df38fe3660
John Pugliesi <jpugliesi12@gmail.com>
2013-05-22 21:04:14 -0700

    First commit

---
+++ TextingApp/build/source/buildConfig/debug/com/example/textingapp/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.textingapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ TextingApp/src/main/java/com/example/textingapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.textingapp;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+
+
+    Button btnSendSMS;
+    EditText txtPhoneNo;
+    EditText txtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSendSMS = (Button) findViewById(R.id.btnSendSMS);
+        txtPhoneNo = (EditText) findViewById(R.id.txtPhoneNo);
+        txtMessage = (EditText) findViewById(R.id.txtMessage);
+
+    }
+
+    public void sendMessage(View view){
+
+
+        String phoneNo = txtPhoneNo.getText().toString();
+        String message = txtMessage.getText().toString();
+
+        sendSMS(phoneNo, message);
+
+    }
+
+    //sends an SMS message to another device
+    private void sendSMS(String phoneNumber, String message) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, null, null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+}
